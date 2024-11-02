a0ad33ba7da9a7ecff171d746246062435d39b3b
VanmathiSelvamani <141158235+VanmathiSelvamani@users.noreply.github.com>
2024-11-02 19:56:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
+// Toggle navigation links on burger click
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
 });
 
-
+// Close the navigation when clicking outside of it
+document.addEventListener('click', (event) => {
+    if (!nav.contains(event.target) && !burger.contains(event.target)) {
+        nav.classList.remove('active');
+        burger.classList.remove('toggle');
+    }
+});
 
 
