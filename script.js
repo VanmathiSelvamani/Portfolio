0871a2375a859459c12e1592006fc4417a6ddc17
VanmathiSelvamani <141158235+VanmathiSelvamani@users.noreply.github.com>
2024-11-02 19:15:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector('.nav-links');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
 });
 
 
